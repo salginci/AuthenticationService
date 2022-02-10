3e9f178ef537db0aa8ea736817e6371356e1e83e
salginci <ozgur@salginci.com>
2022-02-11 01:35:10 +0300

    add test controller

---
+++ Controllers/TestController.cs
@@ -0,0 +1,38 @@
+namespace WebApi.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Helpers;
+using WebApi.Models;
+using WebApi.Services;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    private IUserService _userService;
+
+    public TestController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    
+    [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+
+         
+
+         return Ok( );
+
+
+        }
+
+    
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var users = _userService.GetAll();
+        return Ok(users);
+    }
+}
