e0601f02d9dfd6ba3fa60a72babab96e35cacd96
salginci <ozgur@salginci.com>
2022-02-11 15:19:17 +0300

    bash and docker update

---
+++ bashcommand.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "Web hook triggered. Pulling from repo"
+cd /home/user/AutService/AuthenticationService
+git pull origin master 
+echo "git pull completed"
+
+dotnet publish -c release -o out
+echo "dotnet compile application.! Application is modified as working on port 80"
+
+echo "build docker image"
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=authservice --format="{{.ID}}"))
+docker rmi -f authservice
+echo "remove existing container"
+docker build -t authservice .
+docker run -d -p 9091:80 --name authservice authservice
+echo "Auth service is assigned to port 9091"
